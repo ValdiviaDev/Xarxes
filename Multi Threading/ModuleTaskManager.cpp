9d6c885fcaa486a87284d5f69c91768292da0800
David Valdivia <d.valdiviamartinez@gmail.com>
2020-10-10 17:46:29 +0200

    Finished threads exercise

--- Multi Threading/ModuleTaskManager.cpp
+++ Multi Threading/ModuleTaskManager.cpp
@@ -3,28 +3,32 @@
 
 void ModuleTaskManager::threadMain()
 {
-
 	// TODO 3:
 	// - Wait for new tasks to arrive
 	// - Retrieve a task from scheduledTasks
 	// - Execute it
 	// - Insert it into finishedTasks
 
-	{
-		std::unique_lock<std::mutex> lock(mtx);
+	while(true){
+		{
+			std::unique_lock<std::mutex> lock(mtx);
 
-		while (scheduledTasks.empty()) {
+			while (scheduledTasks.empty()) {
 
-			event.wait(lock);
+				event.wait(lock);
+
+				//When exiting the application, get out of this infinite loop
+				if (exitFlag)
+					return;
+			}
+
+			Task* task_aux = scheduledTasks.front();
+			scheduledTasks.pop();
+
+			task_aux->execute();
+			finishedTasks.push(task_aux);
+			
 		}
-
-		Task* task_aux = scheduledTasks.front();
-
-		scheduledTasks.front()->execute();
-		scheduledTasks.pop();
-		finishedTasks.push(task_aux);
-
-		
 	}
 }
 
@@ -43,12 +47,11 @@ bool ModuleTaskManager::update()
 	// TODO 4: Dispatch all finished tasks to their owner module (use Module::onTaskFinished() callback)
 
 	while (!finishedTasks.empty()) {
-		Module::onTaskFinished(finishedTasks.front());
+		Task* task_aux = finishedTasks.front();
+		task_aux->owner->onTaskFinished(task_aux);
 		finishedTasks.pop();
-		//join?
+
 	}
-	if (!scheduledTasks.empty())
-		event.notify_one();
 
 
 	return true;
@@ -58,10 +61,9 @@ bool ModuleTaskManager::cleanUp()
 {
 	// TODO 5: Notify all threads to finish and join them
 	{
-		std::unique_lock < std::mutex > lock(mtx);
 		exitFlag = true;
 		event.notify_all();
-
+	}
 
 	for (int i = 0; i < MAX_THREADS; i++) {
 		threads[i].join();
@@ -76,8 +78,6 @@ void ModuleTaskManager::scheduleTask(Task* task, Module* owner)
 
 	// TODO 2: Insert the task into scheduledTasks so it is executed by some thread
 	scheduledTasks.push(task);
-	//event.notify_all();
-
-	event.notify_one(); //?¿?¿
+	event.notify_one();
 
 }
